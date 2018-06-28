df52abcf0514b0d473e112504155145d72065cf7
hkdnet <satoko.itse@gmail.com>
2018-06-28 15:18:35 +0900

    cosme

--- lib/ruby_walker/evaluator.rb
+++ lib/ruby_walker/evaluator.rb
@@ -9,34 +9,34 @@ module RubyWalker
 
     def evaluate(node, stack)
       case node.type
-        when 'NODE_SCOPE'
-          # TODO 内容見る
-          return evaluate(node.children[2], stack)
-        when 'NODE_FCALL'
-          mid = node.children[0]
-          args = evaluate(node.children[1], stack)
-          # TODO self は常に Kernel とは限らない
-          if @kernel.respond_to?(mid)
-            return @kernel.public_send(mid, *args)
-          else
-            raise "No such method: Kernel##{mid}"
-          end
-        when 'NODE_ARRAY'
-          return node.children[0..-2].map do |e|
-            evaluate(e, [])
-          end
-        when 'NODE_OPCALL'
-          unless node.children.size == 3
-            raise 'opcall は要素3つだと思ってたけどそうじゃないかも'
-          end
-          recv = evaluate(node.children[0], [])
-          mid = node.children[1]
-          args = evaluate(node.children[2], [])
-          return recv.call(mid, *args)
-        when 'NODE_LIT'
-          return to_literal(node.children.first)
+      when 'NODE_SCOPE'
+        # TODO 内容見る
+        return evaluate(node.children[2], stack)
+      when 'NODE_FCALL'
+        mid = node.children[0]
+        args = evaluate(node.children[1], stack)
+        # TODO self は常に Kernel とは限らない
+        if @kernel.respond_to?(mid)
+          return @kernel.public_send(mid, *args)
         else
-          raise "Unknown node type #{node.type}"
+          raise "No such method: Kernel##{mid}"
+        end
+      when 'NODE_ARRAY'
+        return node.children[0..-2].map do |e|
+          evaluate(e, [])
+        end
+      when 'NODE_OPCALL'
+        unless node.children.size == 3
+          raise 'opcall は要素3つだと思ってたけどそうじゃないかも'
+        end
+        recv = evaluate(node.children[0], [])
+        mid = node.children[1]
+        args = evaluate(node.children[2], [])
+        return recv.call(mid, *args)
+      when 'NODE_LIT'
+        return to_literal(node.children.first)
+      else
+        raise "Unknown node type #{node.type}"
       end
     end
 
@@ -44,10 +44,10 @@ module RubyWalker
 
     def to_literal(val)
       case val
-        when ::Integer
-          RubyWalker::Integer.new(val)
-        else
-          raise "Unknown literal type: #{val.inspect}"
+      when ::Integer
+        RubyWalker::Integer.new(val)
+      else
+        raise "Unknown literal type: #{val.inspect}"
       end
     end
   end
