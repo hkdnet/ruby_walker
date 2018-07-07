5f2d36d2015061e033ae4eba14c2da6e634a9780
hkdnet <satoko.itse@gmail.com>
2018-07-07 20:06:32 +0900

    ::Stray::Builtin::Integer#to_s の引数つきのバージョンを実装

--- lib/stray/builtin/integer.rb
+++ lib/stray/builtin/integer.rb
@@ -13,16 +13,16 @@ module Stray
         ::Stray::Builtin::Integer.new(val + other.val)
       end
 
-      def rb_to_s
-        val.to_s
+      def rb_to_s(base = 10)
+        val.to_s(base)
       end
 
       def class
         ::Stray::Builtin::Integer
       end
 
-      def to_s
-        ::Stray::Builtin::String.new(rb_to_s)
+      def to_s(base = ::Stray::Builtin::Integer.new(10))
+        ::Stray::Builtin::String.new(rb_to_s(base.val))
       end
     end
   end
